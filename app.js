eecf447a6fac121b72c0921d892cede68306cdbf
Nicolas Lujan <nico512lujan@gmail.com>
2024-01-06 08:03:14 -0300

    Fix cors problem x2

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ app.use(cors())
 
 // todos los recursos que sean movies se identifican con /movies
 app.get('/movies', (req, res) => {
-  const origin = req.header('origin')
-  if(ACCEPTED_ORIGINS.includes(origin) || !origin) {
-    res.header('Access-Control-Allow-Origin', origin)
-  }
   const { genre } = req.query
   if (genre) {
     const filteredMovies = movies.filter(
